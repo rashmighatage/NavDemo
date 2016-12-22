7cfce0827fb8b8fca78b900ed9341cef75a558a3
seedcommando_5 <seedcommando_5@SEEDINFOTECH.COM>
2016-12-22 09:21:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/seedcommando_5/navdemo/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.seedcommando_5.navdemo;
+
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    Button find, store;
+    EditText ed;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        find = (Button) findViewById(R.id.btn_search);
+        store = (Button) findViewById(R.id.fav_btn);
+        ed = (EditText) findViewById(R.id.ed_search);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        // BlankFragment b = null;
+        if (id == R.id.nav_gallery) {
+             /*Toast.makeText(this,"In Fragment",Toast.LENGTH_LONG).show();
+             BlankFragment blank=new BlankFragment();
+             android.support.v4.app.FragmentManager m=getSupportFragmentManager();
+             m.beginTransaction().replace(R.id.content_main,blank,blank.getTag()).commit();*/
+            Intent intent = new Intent(this, FavoriteActivity.class);
+
+            startActivityForResult(intent, 1);
+        } else {
+            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void find(View v) {
+        String data = ed.getText().toString();
+        Toast.makeText(this, "" + data, Toast.LENGTH_LONG).show();
+
+
+    }
+
+    public void store(View v) {
+        //String data = ed.getText().toString();
+        Toast.makeText(this, "Favorite", Toast.LENGTH_LONG).show();
+
+
+    }
+    public void store1(View v) {
+        //String data = ed.getText().toString();
+        Toast.makeText(this, "Favorite1", Toast.LENGTH_LONG).show();
+
+
+    }
+}
